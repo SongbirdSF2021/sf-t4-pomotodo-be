947ddfefb5902efe7c25da6609a8ede968374772
Sequita Bridges <sequita.bridges.luol@statefarm.com>
2021-12-10 13:49:33 -0700

    GREEN: adds a todo and returns it with id

--- services/todoDataService.js
+++ services/todoDataService.js
@@ -6,6 +6,7 @@ const TableName = process.env.TABLE_NAME;
 const dynamoClient = require('../db');
 // uuid, useful for generating unique ids
 const uuid = require("uuid");
+const { DynamoDB } = require('aws-sdk');
 
 module.exports = class TodoDataService {
   static async addTodo(todo) {
@@ -18,7 +19,13 @@ module.exports = class TodoDataService {
 
     try {
       // Check the "tododata" table for existing a tododata item
-      // let existingTodoData = ...
+      //var documentClient = new AWS.DynamoDB.DocumentClient();
+      console.log("HERE AT LINE 23")
+      let existingTodoData = await dynamoClient.scan(params).promise()
+      .then((data) => {
+        console.log("EXISTINGTODODATA: ", data);
+        return data
+      })
       
       // no tododata exists yet
       if (existingTodoData.Items.length === 0) {
@@ -37,7 +44,34 @@ module.exports = class TodoDataService {
         }
         // ...
 
-        // Return the newly created tododata item
+       
+     // Return the newly created tododata item
+     await dynamoClient.put(params).promise()
+       .then((data) => {
+         console.log(data);
+       })
+    
+     return await dynamoClient.scan({TableName}).promise().then((data) => {
+       console.log(data.Items[0]); 
+       return data.Items[0];
+      })
+    
+      //another way to do this code using the get to get the same result of the scan
+      //  return await dynamoClient.get({ 
+        //      TableName,
+        //      Key: {
+        //       id: "0"
+        //      }
+        //   }).promise()
+        //   .then((data) => {
+        //     console.log("Latest data:", data.Item);
+        //     return data.Item;
+        //   }).catch((error) => {
+        //     console.log(error)
+        //   });
+
+     
+
       } else { // a tododata item already exist
         existingTodoData = existingTodoData.Items[0];
         existingTodoData.order.push(id);
@@ -58,141 +92,141 @@ module.exports = class TodoDataService {
     }
   }
 
-  static async getTodos() {
-    try {
-      const params = {
-        TableName,
-        Key: {
-          id: "0"
-        }
-      }
+  // static async getTodos() {
+  //   try {
+  //     const params = {
+  //       TableName,
+  //       Key: {
+  //         id: "0"
+  //       }
+  //     }
 
-      // Check the "tododata" table for the tododata item, and return it
-    } catch (error) {
-      console.error(error);
-      return error;
-    }
-  }
+  //     // Check the "tododata" table for the tododata item, and return it
+  //   } catch (error) {
+  //     console.error(error);
+  //     return error;
+  //   }
+  // }
 
-  static async updateOrder(options) {
-    try {
-      const params = {
-        TableName,
-        Key: {
-          id: "0"
-        },
-        // https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.html
-        // UpdateExpression: ...
-        // ExpressionAttributeNames: {
-        //   ...
-        // },
-        // ExpressionAttributeValues: {
-        //   ...
-        // },
-      }
+  // static async updateOrder(options) {
+  //   try {
+  //     const params = {
+  //       TableName,
+  //       Key: {
+  //         id: "0"
+  //       },
+  //       // https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.html
+  //       // UpdateExpression: ...
+  //       // ExpressionAttributeNames: {
+  //       //   ...
+  //       // },
+  //       // ExpressionAttributeValues: {
+  //       //   ...
+  //       // },
+  //     }
 
-      // Update the tododata item
-    } catch (error) {
-      console.error(error);
-      return error;
-    }
-  }
+  //     // Update the tododata item
+  //   } catch (error) {
+  //     console.error(error);
+  //     return error;
+  //   }
+  // }
 
-  static async updateTodo(id, options) {
-    try {
-      let params = {
-        TableName,
-        Key: {
-          id: "0"
-        }
-      }
+  // static async updateTodo(id, options) {
+  //   try {
+  //     let params = {
+  //       TableName,
+  //       Key: {
+  //         id: "0"
+  //       }
+  //     }
 
-      // Check the "tododata" table for the tododata item, and set it to "existingTodo"
-      // let existingTodo = ...
+  //     // Check the "tododata" table for the tododata item, and set it to "existingTodo"
+  //     // let existingTodo = ...
 
-      for (let key in options) {
-        existingTodo.todos[id][key] = options[key];
-      }
+  //     for (let key in options) {
+  //       existingTodo.todos[id][key] = options[key];
+  //     }
 
-      params = {
-        TableName,
-        Item: {
-          ...existingTodo
-        }
-      }
+  //     params = {
+  //       TableName,
+  //       Item: {
+  //         ...existingTodo
+  //       }
+  //     }
 
-      // Replace the existing tododata item with the updated one
-    } catch (error) {
-      console.error(error);
-      return error;
-    }
-  }
+  //     // Replace the existing tododata item with the updated one
+  //   } catch (error) {
+  //     console.error(error);
+  //     return error;
+  //   }
+  // }
 
-  static async deleteTodo(id) {
-    try {
-      let params = {
-        TableName,
-        Key: {
-          id: "0"
-        }
-      }
+  // static async deleteTodo(id) {
+  //   try {
+  //     let params = {
+  //       TableName,
+  //       Key: {
+  //         id: "0"
+  //       }
+  //     }
 
-      // Check the "tododata" table for the tododata item, and set it to "existingTodo"
-      // let existingTodo = ...
+  //     // Check the "tododata" table for the tododata item, and set it to "existingTodo"
+  //     // let existingTodo = ...
 
-      existingTodo.order = existingTodo.order.filter((orderId) => {
-        return orderId !== id
-      });
+  //     existingTodo.order = existingTodo.order.filter((orderId) => {
+  //       return orderId !== id
+  //     });
 
-      delete existingTodo.todos[id];
+  //     delete existingTodo.todos[id];
 
-      params = {
-        TableName,
-        Item: {
-          ...existingTodo
-        }
-      }
+  //     params = {
+  //       TableName,
+  //       Item: {
+  //         ...existingTodo
+  //       }
+  //     }
 
-      // Replace the existing tododata item with the updated one
-    } catch (error) {
-      console.error(error);
-      return error;
-    }
-  }
+  //     // Replace the existing tododata item with the updated one
+  //   } catch (error) {
+  //     console.error(error);
+  //     return error;
+  //   }
+  // }
 
-  static async deleteCompletedTodos() {
-    try {
-      let params = {
-        TableName,
-        Key: {
-          id: "0"
-        }
-      }
+  // static async deleteCompletedTodos() {
+  //   try {
+  //     let params = {
+  //       TableName,
+  //       Key: {
+  //         id: "0"
+  //       }
+  //     }
 
-      let existingTodo = await dynamoClient.scan(params).promise().then((data) => {
-          return data.Items[0];
-      });
+  //     let existingTodo = await dynamoClient.scan(params).promise().then((data) => {
+  //         return data.Items[0];
+  //     });
 
-      existingTodo.order = existingTodo.order.filter((orderId) => {
-        return !existingTodo.todos[orderId].completed;
-      });
-      for (let id in existingTodo.todos) {
-        if (existingTodo.todos[id].completed) {
-          delete existingTodo.todos[id];
-        }
-      }
+  //     existingTodo.order = existingTodo.order.filter((orderId) => {
+  //       return !existingTodo.todos[orderId].completed;
+  //     });
+  //     for (let id in existingTodo.todos) {
+  //       if (existingTodo.todos[id].completed) {
+  //         delete existingTodo.todos[id];
+  //       }
+  //     }
       
-      params = {
-        TableName,
-        Item: {
-          ...existingTodo
-        }
-      }
+  //     params = {
+  //       TableName,
+  //       Item: {
+  //         ...existingTodo
+  //       }
+  //     }
 
-      await dynamoClient.put(params).promise();
-    } catch (error) {
-      console.error(error);
-      return error;
-    }
-  }
+  //     await dynamoClient.put(params).promise();
+  //   } catch (error) {
+  //     console.error(error);
+  //     return error;
+  //   }
+  // }
 };
